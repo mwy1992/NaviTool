43352544098d578e657a3953ff3e29a58efb9c6f
mwy1992 <mwy1992@gmail.com>
2025-12-06 16:00:49 +0800

    Delete app/libs/ecarx.adaptapi.jar.src directory

--- app/libs/ecarx.adaptapi.jar.src/com/ecarx/xui/adaptapi/dvr/IDvrPhotoFile.java
+++
@@ -1,9 +0,0 @@
-package com.ecarx.xui.adaptapi.dvr;
-
-public interface IDvrPhotoFile extends IDvrFile {}
-
-
-/* Location:              C:\Users\mwy19\Desktop\Monjaro\jd-gui-windows-1.6.6\ecarx.adaptapi.jar!\com\ecarx\xui\adaptapi\dvr\IDvrPhotoFile.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
--- app/libs/ecarx.adaptapi.jar.src/com/ecarx/xui/adaptapi/navigation/ehp/v2/IHznProfLongLRMessage.java
+++
@@ -1,9 +0,0 @@
-package com.ecarx.xui.adaptapi.navigation.ehp.v2;
-
-public interface IHznProfLongLRMessage extends IHznProfLongMessage {}
-
-
-/* Location:              C:\Users\mwy19\Desktop\Monjaro\jd-gui-windows-1.6.6\ecarx.adaptapi.jar!\com\ecarx\xui\adaptapi\navigation\ehp\v2\IHznProfLongLRMessage.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
